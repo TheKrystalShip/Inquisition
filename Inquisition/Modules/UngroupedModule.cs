60c22239b724b6555c5a6eed46f33a447b579bd3
Cristian Moraru <cristian.moraru@live.com>
2017-12-07 12:36:31 +0100

    Removed ShowModule and moved all command to Ungrouped instead, reducing the commands to just basic requests.

--- Inquisition/Modules/UngroupedModule.cs
+++ Inquisition/Modules/UngroupedModule.cs
@@ -151,5 +151,141 @@ namespace Inquisition.Modules
                 Console.WriteLine(ex.Message);
             }
         }
+
+        [Command("joke")]
+        [Alias("joke by")]
+        public async Task ShowJokeAsync(SocketUser user = null)
+        {
+            List<Joke> Jokes;
+            Random rn = new Random();
+
+            if (user is null)
+            {
+                Jokes = DbHandler.ListAll(new Joke());
+                user = Context.User;
+            }
+            else
+            {
+                Jokes = DbHandler.ListAll(new Joke(), user);
+            }
+
+            try
+            {
+                Joke joke = Jokes[rn.Next(Jokes.Count)];
+                EmbedBuilder embed = EmbedTemplate.Create(Context.Client.CurrentUser, Context.User);
+                embed.WithDescription($"Submitted by {joke.AuthorName}:");
+                embed.AddField($"{joke.Text}", $"P:{joke.PositiveVotes} - N:{joke.NegativeVotes}");
+
+                await ReplyAsync($"Here you go:", false, embed.Build());
+            }
+            catch (Exception ex)
+            {
+                await ReplyAsync($"Something happened, oops. Let the admin know pls thx <3");
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        [Command("meme")]
+        [Alias("meme by")]
+        public async Task ShowMemeAsync(SocketUser user = null)
+        {
+            List<Meme> Memes;
+            Random rn = new Random();
+
+            if (user is null)
+            {
+                Memes = DbHandler.ListAll(new Meme());
+                user = Context.User;
+            }
+            else
+            {
+                Memes = DbHandler.ListAll(new Meme(), user);
+            }
+
+            try
+            {
+                Meme meme = Memes[rn.Next(Memes.Count)];
+                EmbedBuilder embed = EmbedTemplate.Create(Context.Client.CurrentUser, Context.User);
+                embed.WithDescription($"Submitted by {meme.AuthorName}:");
+                embed.WithImageUrl(meme.Url);
+
+                await ReplyAsync($"Here you go:", false, embed.Build());
+            }
+            catch (Exception ex)
+            {
+                await ReplyAsync($"Something happened, oops. Let the admin know pls thx <3");
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+
+    [Group("game")]
+    public class GameModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("status")]
+        [Alias("info")]
+        [Summary("Returns if a game server is online")]
+        public async Task StatusAsync(string name)
+        {
+            Data.Game game = DbHandler.GetFromDb(new Data.Game { Name = name });
+            if (game is null)
+            {
+                await ReplyAsync(Message.Error.GameNotFound(game));
+                return;
+            }
+
+            bool ProcessRunning = ProcessDictionary.Instance.TryGetValue(game.Name, out Process p);
+            bool GameMarkedOnline = game.IsOnline;
+
+            if (!ProcessRunning && !GameMarkedOnline)
+            {
+                await ReplyAsync($"{game.Name} server is offline. If you wish to start it up use: game start \"{game.Name}\"");
+                return;
+            }
+            else if (ProcessRunning && !GameMarkedOnline)
+            {
+                await ReplyAsync($"{game.Name} has a process running, but is marked as offline in the database, " +
+                    $"please let the knobhead who programmed this know abut this error, thanks");
+                return;
+            }
+            else if (!ProcessRunning && GameMarkedOnline)
+            {
+                await ReplyAsync($"{game.Name} server is not running, but is marked as online in the database, " +
+                    $"please let the knobhead who programmed this know abut this error, thanks");
+                return;
+            }
+
+            await ReplyAsync($"{game.Name} server is online, version {game.Version} on port {game.Port}");
+        }
+
+        [Command("version")]
+        [Summary("Returns a game's version")]
+        public async Task GameVersionAsync(string name)
+        {
+            Data.Game game = DbHandler.GetFromDb(new Data.Game { Name = name });
+            if (game is null)
+            {
+                await ReplyAsync(Message.Error.GameNotFound(game));
+                return;
+            }
+
+            await ReplyAsync($"{game.Name}'s version is {game.Version}");
+        }
+
+        [Command("port")]
+        [Summary("Returns a game's port")]
+        public async Task GamePortAsync(string name)
+        {
+            Data.Game game = DbHandler.GetFromDb(new Data.Game { Name = name });
+            if (game is null)
+            {
+                await ReplyAsync(Message.Error.GameNotFound(game));
+                return;
+            }
+
+            await ReplyAsync($"{game.Name}'s port is {game.Port}");
+        }
     }
 }
