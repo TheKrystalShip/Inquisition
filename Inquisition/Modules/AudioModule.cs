a9d0ab728299826103739ae0f2223da977b4abe9
Cristian Moraru <cristian.moraru@live.com>
2017-12-13 12:52:54 +0100

    Removed unnecessary Discord.Net.Rest package, audio still not working, bot connects but instantly leaves. Official documentation sample code doesn't work, same effect.

--- Inquisition/Modules/AudioModule.cs
+++ Inquisition/Modules/AudioModule.cs
@@ -1,12 +1,14 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Discord.Audio;
 using Inquisition.Data;
 using Inquisition.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Inquisition.Modules
 {
@@ -24,27 +26,29 @@ namespace Inquisition.Modules
         public async Task JoinVoiceChannelAsync()
         {
             SocketVoiceChannel voiceChannel = (Context.User as SocketGuildUser).VoiceChannel;
+
             if (voiceChannel is null)
             {
                 await ReplyAsync(Message.Error.NotInVoiceChannel);
                 return;
             }
 
-            await _service.JoinAudio(Context.Guild, voiceChannel);
+            await voiceChannel.ConnectAsync();
         }
 
-        [Command("leave", RunMode = RunMode.Async)]
+        [Command("leave")]
         [Summary("Kick the bot from the voice channel")]
-        public async Task LeaveCmd()
+        public async Task LeaveVoiceChannelAsync()
         {
             SocketVoiceChannel voiceChannel = (Context.User as SocketGuildUser).VoiceChannel;
+
             if (voiceChannel is null)
             {
                 await ReplyAsync(Message.Error.NotInVoiceChannel);
                 return;
             }
 
-            await _service.LeaveAudio(Context.Guild);
+            await voiceChannel.
         }
     }
 }
