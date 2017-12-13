16ee0a4cf66852694ea816a81dfb6f2bc8b8a21c
Cristian Moraru <cristian.moraru@live.com>
2017-12-13 11:35:27 +0100

    Added back AudioModule and Service, commands to Join/Leave are in place and linked to the service. [Bug] The bot joins the voice channel but leaves almost instantly after, so it's still not working as intended.

---
+++ Inquisition/Modules/AudioModule.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Inquisition.Data;
+using Inquisition.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inquisition.Modules
+{
+    public class AudioModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly AudioService _service;
+
+        public AudioModule(AudioService service)
+        {
+            _service = service;
+        }
+
+        [Command("join", RunMode = RunMode.Async)]
+        [Summary("Make the bot join your voice channel")]
+        public async Task JoinVoiceChannelAsync()
+        {
+            SocketVoiceChannel voiceChannel = (Context.User as SocketGuildUser).VoiceChannel;
+            if (voiceChannel is null)
+            {
+                await ReplyAsync(Message.Error.NotInVoiceChannel);
+                return;
+            }
+
+            await _service.JoinAudio(Context.Guild, voiceChannel);
+        }
+
+        [Command("leave", RunMode = RunMode.Async)]
+        [Summary("Kick the bot from the voice channel")]
+        public async Task LeaveCmd()
+        {
+            SocketVoiceChannel voiceChannel = (Context.User as SocketGuildUser).VoiceChannel;
+            if (voiceChannel is null)
+            {
+                await ReplyAsync(Message.Error.NotInVoiceChannel);
+                return;
+            }
+
+            await _service.LeaveAudio(Context.Guild);
+        }
+    }
+}
---
+++ Inquisition/Services/AudioService.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.Audio;
+using Discord.WebSocket;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inquisition.Services
+{
+    public class AudioService
+    {
+        private readonly ConcurrentDictionary<ulong, IAudioClient> ConnectedChannels = new ConcurrentDictionary<ulong, IAudioClient>();
+
+        public async Task JoinAudio(SocketGuild guild, SocketVoiceChannel target)
+        {
+            if (ConnectedChannels.TryGetValue(guild.Id, out IAudioClient client))
+            {
+                return;
+            }
+            if (target.Guild.Id != guild.Id)
+            {
+                return;
+            }
+
+            var audioClient = await target.ConnectAsync();
+        }
+
+        public async Task LeaveAudio(SocketGuild guild)
+        {
+            if (ConnectedChannels.TryRemove(guild.Id, out IAudioClient client))
+            {
+                await client.StopAsync();
+            }
+        }
+    }
+}
